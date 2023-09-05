6504deabc723059741e9933f6abcd6f2c0bce531
golbangi1000 <kenjames777@naver.com>
2023-09-06 01:53:10 +0900

    faw
    awf

--- TheOdinPractice/practice1.js
+++ TheOdinPractice/practice1.js
@@ -10,3 +10,7 @@ console.log('awefawef')
 let x = ((0.1 * 10 + 0.2*10) / 10)
 
 console.log(x)
+
+const myText = "I am a string";
+const newString = myText.replace("string", "sausage");
+console.log(newString);
