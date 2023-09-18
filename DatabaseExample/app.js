1a7628369340161f1391947660967440839389e7
golbangi1000 <kenjames777@naver.com>
2023-09-18 21:53:29 +0900

    waefaw
    efawefwae

--- DatabaseExample/app.js
+++ DatabaseExample/app.js
@@ -1,151 +1,273 @@
+//mongoose 모듈 사용하기
 
-const express = require('express');
-const http = require('http');
+//Express 기본 모듈
+require("dotenv").config();
+var express = require("express");
+var http = require("http");
+var path = require("path");
+//var bodyParser = require("body-parser"); //예전에는 bodyParser를 호출해야 했지만 express에 내장이 되어 안써도 괜춘
+var serveStatic = require("serve-static"); //특정 폴더를 패스로 접근 가능하게 하는것.
+var expressErrorHandler = require("express-error-handler");
+var expressSession = require("express-session");
 
-const static = require('serve-static');
-const path = require('path');
+//몽고디비 모듈
+//var MongoClient = require("mongodb").MongoClient; 몽구스모듈을 쓰면 이렇게 할 필요 없어서 여긴 주석
 
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
+//mongoose 모듈
+var mongoose = require("mongoose");
 
-const expressSession = require('express-session');
-const mongoose = require('mongoose')
+//데이터베이스 객체를 위한 변수
+var database; //==connection conn; 과 같음
 
-//에러 핸들러 모듈 사용
-const expressErrorHandler = require('express-error-handler');
+//데이터베이스 스키마를 위한 변수
+var UserSchema;
 
-// mongodb 모듈 사용
+//데이터베이스 모델을 위한 변수
+var UserModel;
 
- const MongoClient = require('mongodb').MongoClient;
+//데이터베이스 연결
+function connectDB(){
 
- var database;
-    var databaseUrl = 'mongodb://127.0.0.1:27017'
-    MongoClient.connect(databaseUrl,(err, client) => {
-        if (err) {
-            console.log('DB 에러 발생!!')
-        }
-        console.log('DB 연결됨 :  ' + databaseUrl)
-        
-        database = client.db("local")
-        app.listen(3000, function(){
-            console.log('db connected')
-        })
-        
-    })
+	//데이터베이스 연결 정보
+	var databaseUrl = "mongodb://127.0.0.1:27017/local";
 
-
-
-
-
-const app = express();
-
-
-
-app.set('port', process.env.PORT || 3000);
-//미리 만들어진 미들웨어를 쓰기 static 미들웨어
-//이렇게 /public 앞에 붙히면 앞에 public을 쳐야됨 뺴면 안침
-app.use('/public', static(path.join(__dirname, 'public')));
-
-
-//body parser 대신 express씀 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-app.use(cookieParser());
-
-app.use(expressSession({
-    secret: 'my key',
-    resave: true,
-    saveUninitialized: true
-}));
-
-
-var router = express.Router();
-
-router.route('/process/login').post(function(req,res){
-    console.log('/process/login 라우팅 함수 호출됨');
-
-    const paramId = req.body.id || req.query.id;
-    const paramPassword = req.body.password || req.query.password;
-
-    console.log('요청한 파라미터 : ' + paramId + ', '  + paramPassword);
-
-    if(database){
-        authUser(database,paramId,paramPassword, function(err,docs){
-            if(err){
-                console.log('에러발생1');
-                res.writeHead(200,{"Content-Type":"text/html;charset=utf8"});
-                res.write('<h1>에러 발생</h1>');
-                res.end();
-                return; 
-            }
-
-            if(docs){
-                console.dir(docs);
-                res.writeHead(200,{"Content-Type":"text/html;charset=utf8"});
-                res.write('<h1>사용자 로그인 성공</h1>');
-                res.write('<div><p>사용자 : ' + docs[0].name + '</p></div>')
-                res.write('<br><br> <a href = "/public/login.html> 다시 로그인 하기</a>')
-                res.end();
-                
-
-            } else{
-                console.log('에러발생2');
-                res.writeHead(200, {"Content-Type" : "text/html;charset=utf8"});
-                res.write('<h1> 데이터 없음 </h1>')
-                res.end();
-                
-            }
-        });
-    }   else{
-        console.log('에러발생3');
-            res.writeHead(200, {"Content-Type" : "text/html;charset=utf8"});
-            res.write('<h1> 데이터베이스 연결안됨 </h1>')
-            res.end();
-                
-    }
-
-});
-
-app.use('/', router);
-
-
-const authUser = function(db, id , password, callback) {
-    console.log('authUser 호출됨'+ id + ', ' + password);
-
-    let users = db.collection('users');
-
-    users.find({"id":id,"password":password}).toArray(function(err,docs){
-        if(err){
-            callback(err,null);
-            return;
-        }
-        if(docs.length > 0){
-            console.log('일치하는 사용자를 찾음');
-            callback(null,docs);
-        } else{
-            console.log('일치하는 사용자를 찾지못함')
-            callback(null, null);
-        }
-    });
+	//연결
+	mongoose.connect(databaseUrl); //몽고DB의 정보를 몽구스모듈과 연결해준다.
+	
+	database = mongoose.connection;
+	
+	database.on("open",function(){//open이라는 내장이벤트 db가 열려있냐
+		
+		console.log(" 데이터베이스가 연결 되었습니다: " + databaseUrl); //여기까지왔으면 DB연결 성공
+		
+		//스키마 정의
+		UserSchema = mongoose.Schema({
+			id:String,
+			name:String,
+			pwd:String
+			
+		});
+		
+		console.log("UserSchema 정의함.");
+		
+		//Model 정의 - 스키마를 정의했으면 Model를 정의해야 함
+		UserModel = mongoose.model("users",UserSchema);//users 테이블에 UserSchema를 적용해라
+		
+		console.log("UserModel 정의함.");
+		
+	}); 
+						//function자리에 한번에써준것.
+	database.on("error",console.error.bind(console,"몽구스 모듈 에러")); //이렇게 한줄로 써줄수도있다.
+									
+	database.on("disconnected",function(){
+		
+		console.log("DB연결이 끊겼습니다 5초후 재연결 합니다.");
+		setInterval(connectDB(),5000); //디비연결이 끊기면 5초마다 다시 연결하는 함수를 실행
+	});
 }
 
+//익스프레스 객체 생성
+var app = express();
+
+app.set("port",process.env.PORT||3000);
+
+app.use(express.urlencoded({extended:false}));
+
+app.use("/public",serveStatic(path.join(__dirname,"public"))); //public (실제)폴더의 이름을 써준것
+//사용자정의
+
+app.use(expressSession({
+
+	secret:"my key",
+	resave:true,
+	saveUninitialized:true
+
+}));
+
+//작업하는 함수를 만들고 그걸 불러쓰는 라우터를 만듬.
+//사용자를 인증하는 함수
+var authUser = function(database,id,pwd,callback){ //위에서 만든 database
+
+	console.log("addUser 함수 호출");
+
+	//아이디와 비밀번호 검색할것. 여러개나올 수 있으니 배열로 받음
+	// UserModel.find({"id":id,"pwd":pwd},function (err,result){
+		
+	// 	//스키마와 모델을 정의해준값에 의해 UserModel에 users가 들어있는것
+	// 	if(err){
+	// 		callback(err,null); //에러가 있으면 에러 출력후 스톱
+	// 		return;
+	// 	}
+
+	// 	//데이터가 있을경우
+	// 	if(result.length>0){
+
+	// 		callback(null,result); //데이터가 있으면 데이터 전송
+
+	// 	}else{
+
+	// 		console.log("일치하는 데이터가 없습니다.");
+	// 		callback(null,null); //에러도 없으니깐 null,데이터도없으니깐 null
+	// 	}
+	// });
+    UserModel.find({"id":id,"pwd":pwd})
+    .then(function (err){
+        callback(null,result); //데이터가 있으면 데이터 전송
+    })
+    .catch(function (result) {
+        console.log(err);
+      });
+}
+
+//사용자를 추가하는 함수
+var addUser = function(database,id,pwd,name,callback){
+
+	console.log("addUser 함수 호출");
+
+	var users = new UserModel({"id":id,"pwd":pwd,"name":name});
+
+	users.save(function(err,result){
+
+		if(err){
+			callback(err,null);
+			return;
+		}
+
+		if(result){
+
+			console.log("사용자 추가~");
+			
+
+		}else{
+
+			console.log("사용자 추가 실패 ㅠ-ㅠ");
+		}
+
+		callback(null,result);
+	});
+
+};
+
+//-----------------------------------------------------------------------------
+//라우터 객체 생성
+var router = express.Router();
+
+//로그인 라우터
+router.route("/process/login").post(function(req,res){
+
+	console.log("/process/login 호출");
+
+	var id = req.body.id;
+	var pwd = req.body.pwd;
+
+	if(database){
+		//callback함수가 되는것
+		authUser(database, id, pwd, function(err,result){ //위 사용자 인증 함수로 올라감
+
+			if(err) throw err;
+
+			if(result){ //데이터가 있으면
+
+				var userName = result[0].name;//위에서 데이터를 배열로 받기로 했슴
+
+				res.writeHead("200",{"Content-type":"text/html;charset=utf-8"});
+				res.write("<h1>로그인 성 공 ~ ~</h1>");
+				res.write("<div>아이디: " + id + "</div>");
+				res.write("<div>이름: " + userName + "</div>");
+				res.write("<br><br><a href='/public/login.html'>다시 로그인</a>");
+				res.end();
+
+			}else{
+
+				res.writeHead("200",{"Content-type":"text/html;charset=utf-8"});
+				res.write("<h1>로그인 실 패 ㅠ-ㅠ</h1>");
+				res.write("<div>아이디또는 패스워드를 확인하세요</div>");
+				res.write("<br><br><a href='/public/login.html'>다시 로그인</a>");
+				res.end();
+
+			}
+
+		});
+
+
+	}else{
+
+		res.writeHead("200",{"Content-type":"text/html;charset=utf-8"});
+		res.write("<h1>DB연결 실 패 ㅠ-ㅠ</h1>");
+		res.write("<div>DB를 연결하지 못했습니다</div>");
+		res.end();
+
+	}
 
 
 
+});
+
+router.route("/process/addUser").post(function(req,res){
+
+	console.log("/process/addUser 호출");
+
+	var id = req.body.id;
+	var pwd = req.body.pwd;
+	var name = req.body.name;
+
+	if(database){
+
+		addUser(database,id,pwd,name, function(err,result){ //함수속 매개변수는 사용자 정의
+
+			if(err) throw err;
+
+			if(result){
+
+				res.writeHead("200",{"Content-type":"text/html;charset=utf-8"});
+				res.write("<h1>사용자 추가 성공</h1>");
+				res.end();
 
 
-//404 에러 페이지 처리 
-const errorHandler = expressErrorHandler({
-    static: {
-        '404': './public/404.html'
-    }
+			}else{
+
+				res.writeHead("200",{"Content-type":"text/html;charset=utf-8"});
+				res.write("<h1>사용자 추가 실패</h1>");
+				res.end();
+
+			}
+
+		});
+
+	}else{
+
+		res.writeHead("200",{"Content-type":"text/html;charset=utf-8"});
+		res.write("<h1>DB연결 실 패 ㅠ-ㅠ</h1>");
+		res.write("<div>DB를 연결하지 못했습니다</div>");
+		res.end();
+
+	}
+
 });
 
 
-//데이터베이스는 웹서버 실행되는 상태를 확인하고 database실행 
-const server = http.createServer(app).listen(app.get('port'), function () {
-    console.log('익스프레스로 웹서버를 실행함 : ' + app.get('port'));
+//라우터 객체 등록
+app.use("/",router);
 
-    // connectDB();
-})
+var errorHandler = expressErrorHandler({
+
+	static: { //미리 메모리상에 올려둔것
+		"404":"./public/404.html" //404에러가 뜨면 public에 404.html로 가라
+	}
+});
+
+app.use(expressErrorHandler.httpError(404));
+app.use(errorHandler); //변수명 담아줌
+
+
+//Express 서버 시작
+//var server = http.createServer();
+
+http.createServer(app).listen(app.get("port"),function(){ //위에 set으로 넣어둔 port를 get으로 가져온거
+
+	console.log("익스프레스 서버 on~~ 포트번호는:" + app.get("port"));
+
+	//DB연결 함수 호출
+	connectDB(); 
+
+});
