5ab627f51ce2cef44fa23f05d5c7ce23122d02a0
golbangi1000 <kenjames777@naver.com>
2023-10-03 00:35:10 +0900

    weaf
    awef

--- TheOdinPractice/practice1.js
+++ TheOdinPractice/practice1.js
@@ -14,3 +14,24 @@ console.log(x)
 const myText = "I am a string";
 const newString = myText.replace("string", "sausage");
 console.log(newString);
+
+
+// const a = 'a';
+// const b = 'b';
+// const d = undefined;
+// let c = d || b;
+// console.log(c);
+
+
+console.log((true && false) || true);
+
+let a = 'red';
+let b = 'blue';
+
+let c;
+
+c = a
+a = b
+b = c
+
+console.log(a + '::'+ b)
---
+++ TheOdinPractice/practice2.js
@@ -0,0 +1,25 @@
+let role = 69;
+
+switch(role){
+
+    case'guest':{
+
+        console.log('guest user')
+        break;
+    }
+
+    case 'moderator':{
+
+        console.log('mod user')
+        break;
+    }
+    
+    case 69:{
+        console.log(69)
+        break;
+    }
+    default:{
+        console.log('unknown user')
+    }
+}
+
