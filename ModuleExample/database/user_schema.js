622fdbadcb15b2bbadbca8cf8ce966e8a720cd53
golbangi1000 <kenjames777@naver.com>
2023-10-09 21:55:50 +0900

    waef
    weaf

--- ModuleExample/database/user_schema.js
+++ ModuleExample/database/user_schema.js
@@ -1,62 +1,78 @@
+import crypto from 'crypto';
+
+
+
 var Schema = {};
 
-Schema.createSchema = function () {
+
+Schema.createSchema = function (mongoose) {
+
+   console.log('createSchema 호출됨')
    //스키마 정의
-   UserSchema = mongoose.Schema({
-      id: { type: String, required: true, unique: true, default: "" },
-      hashed_password: { type: String, require: true, default: "" },
-      salt: { type: String, reqruired: true },
-      name: { type: String, index: "hashed", default: "" },
-      age: { type: Number, default: -1 },
-      created_at: { type: Date, index: { unique: false }, default: Date.now() },
-      updated_at: { type: Date, index: { unique: false }, default: Date.now() },
-   });
+   var UserSchema = mongoose.Schema({
+      id:{type: String, required: true, unique: true, default:''},
+      hashed_password: {type: String , require: true, default:''},
+      salt: {type: String, reqruired: true},
+   name:{type: String , index: 'hashed', default:''},
+   age: {type : Number, default:-1},
+      created_at:{type:Date, index: {unique:false}, default:Date.now()},
+      updated_at:{type:Date, index: {unique:false}, default:Date.now()}
+});
 
-   console.log("UserSchema 정의함.");
-   F;
+console.log("UserSchema 정의함.");
 
-   UserSchema.virtual("password")
-      //파라미터로 password라는것을 받고
-      .set(function (password) {
-         this.salt = this.makeSalt();
-         this.hashed_password = this.encryptPassword(password);
-         console.log("virtual password 저장됨" + this.hashed_password);
-      });
-
-   UserSchema.method("encryptPassword", function (plainText, inSalt) {
-      if (inSalt) {
-         //-----------------sha1알고리즘과 digest 알고리즘
-         //salt값은 일종의 시드값 같은거
-         return crypto.createHmac("sha1", inSalt).update(plainText).digest("hex");
-      } else {
-         return crypto.createHmac("sha1", this.salt).update(plainText).digest("hex");
+  UserSchema.virtual('password')
+              //파라미터로 password라는것을 받고 
+  .set(function(password){
+      this.salt = this.makeSalt();
+      this.hashed_password = this.encryptPassword(password);
+      console.log('virtual password 저장됨' + this.hashed_password);
+  });
+  
+  UserSchema.method('encryptPassword', function(plainText, inSalt){
+      if(inSalt) {
+          //-----------------sha1알고리즘과 digest 알고리즘 
+          //salt값은 일종의 시드값 같은거 
+          return crypto.createHmac('sha1', inSalt).update(plainText).digest('hex');
+      } else{
+          return crypto.createHmac('sha1',this.salt).update(plainText).digest('hex');
       }
-   });
+  });
+  
 
-   //makeSalt는 특정한값을 랜덤하게 만드는거 결국에는 매번 salt를 사용하면 새로운값이 만들어지겠지
-   UserSchema.method("makeSalt", function () {
-      // + ''이건 문자열 변화용
-      return Math.round(new Date().valueOf() * Math.random()) + "";
-   });
+  //makeSalt는 특정한값을 랜덤하게 만드는거 결국에는 매번 salt를 사용하면 새로운값이 만들어지겠지
+  UserSchema.method('makeSalt',function(){            // + ''이건 문자열 변화용
+      return Math.round((new Date().valueOf() * Math.random())) + '';
+  });
 
-   UserSchema.method("authenticate", function (plainText, inSalt, hashed_password) {
-      if (inSalt) {
-         console.log("authenticate 호출됨");
-         return this.encryptPassword(plainText, inSalt) === hashed_password;
+
+  UserSchema.method('authenticate', function(plainText,inSalt,hashed_password){
+      if(inSalt){
+          console.log('authenticate 호출됨');
+          return this.encryptPassword(plainText, inSalt) === hashed_password;
+
       } else {
-         console.log("authenticate 호출됨");
-         return this.encryptPassword(plainText) === hashed_password;
-      }
-   });
+          console.log('authenticate 호출됨');
+          return this.encryptPassword(plainText) === hashed_password;
 
-   UserSchema.static("findById", async function (id) {
-      const user = await this.find({ id: id });
+      }
+  })
+  // UserSchema.static('findById', function(id, callback){
+  //     return this.find({id:id}, callback);
+  // });
+
+  UserSchema.static('findById', async function(id) {
+      const user = await this.find({id:id});
       return user;
-   });
+    });
 
-   UserSchema.static("findAll", async function () {
-      return this.find({});
-   });
+  
+  //이런 
+UserSchema.static('findAll', async function(){
+   return this.find({});
+})
+
+    return UserSchema;
 };
 
-export default Schema;
+export default  Schema;
---
+++ ModuleExample/routes/user.js
@@ -0,0 +1,150 @@
+
+
+const login = function(req,res){
+
+	console.log("/process/login 호출");
+
+	var id = req.body.id;
+	var pwd = req.body.pwd;
+                        //app는 또 어딨냐? req에 넣어주면된다
+    const database  = req.app.get('database');
+
+	if(database){
+		//callback함수가 되는것
+		authUser(database, id, pwd, function(err,result){ //위 사용자 인증 함수로 올라감
+
+			if(err) throw err;
+
+			if(result){ //데이터가 있으면
+
+				var userName = result[0].name;//위에서 데이터를 배열로 받기로 했슴
+				
+				res.writeHead("200",{"Content-type":"text/html;charset=utf-8"});
+				res.write("<h1>로그인 성 공 ~ ~</h1>");
+				res.write("<div>아이디: " + id + "</div>");
+				res.write("<div>이름: " + userName + "</div>");
+				res.write("<br><br><a href='/public/login.html'>다시 로그인</a>");
+				res.end();
+
+			}else{
+
+				res.writeHead("200",{"Content-type":"text/html;charset=utf-8"});
+				res.write("<h1>로그인 실 패 ㅠ-ㅠ</h1>");
+				res.write("<div>아이디또는 패스워드를 확인하세요</div>");
+				res.write("<br><br><a href='/public/login.html'>다시 로그인</a>");
+				res.end();
+
+			}
+
+		});
+
+
+	}else{
+
+		res.writeHead("200",{"Content-type":"text/html;charset=utf-8"});
+		res.write("<h1>DB연결 실 패 ㅠ-ㅠ</h1>");
+		res.write("<div>DB를 연결하지 못했습니다</div>");
+		res.end();
+
+	}
+
+
+
+
+};
+
+
+const addUser = function(req,res){
+
+	console.log("/process/addUser 호출");
+
+	var id = req.body.id;
+	var pwd = req.body.pwd;
+	var name = req.body.name;
+    const database  = req.app.get('database');
+
+	if(database){
+
+		addUser(database,id,pwd,name, function(err,result){ //함수속 매개변수는 사용자 정의
+
+			if(err) throw err;
+
+			if(result){
+
+				res.writeHead("200",{"Content-type":"text/html;charset=utf-8"});
+				res.write("<h1>사용자 추가 성공</h1>");
+				res.write("<h2>사용자 ID" + id+"이름:"+ name+ "</h2>")
+				res.end();
+
+
+			}else{
+
+				res.writeHead("200",{"Content-type":"text/html;charset=utf-8"});
+				res.write("<h1>사용자 추가 실패</h1>");
+				res.end();
+
+			}
+
+		});
+
+	}else{
+
+		res.writeHead("200",{"Content-type":"text/html;charset=utf-8"});
+		res.write("<h1>DB연결 실 패 ㅠ-ㅠ</h1>");
+		res.write("<div>DB를 연결하지 못했습니다</div>");
+		res.end();
+
+	}
+
+};
+
+
+const listUser = function(req,res){
+    console.log('/process/listure 라우팅함수 호출됨');
+    const database  = req.app.get('database');
+
+	//집에서 해보기
+    if(database){
+        database.UserModel.findAll(function(err,results){
+            if(err){
+                console.log('에러발생')
+                res.writeHead(200, {"Content-Type":"text/html;charset=utf8"});
+                res.write('<h1>에러발생</h1>')
+                res.end();
+                return;
+            }
+
+            if(results){
+                console.dir(results);
+
+                res.writeHead(  200, {"Content-type:": "text/html;charset=utf8"})
+                res.write("<h3>사용자 리스트</h3>")
+                res.write("<div><ul>");
+
+                for(var i = 0; i < results.length; i++){
+                    var curId = results[i]._doc.id;
+                    var curName = results[i]._doc.name;
+                    res.write(" <li>#" + i + " -> " + curId + ", " + curName+ "</li>" )
+
+                }
+
+                res.write("</ul></div>")
+                res.end();
+            } else {
+                console.log('에러발생')
+                res.writeHead(200, {"Content-Type":"text/html;charset=utf8"});
+                res.write('<h1>조회된 사용자 없음</h1>')
+                res.end();
+            }
+        });
+    } else{
+        console.log('에러발생');
+        res.writeHead(200, {"Content-Type":"text/html;charset=utf8"});
+        res.write("<h1>데이터베이스 연결 안됨</h1>");
+        res.end();
+    }
+
+}
+
+export {login,addUser,listUser};
+
