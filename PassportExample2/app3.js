4a1a8fad599aa670dc6190a2054c6e279dd57a38
golbangi1000 <kenjames777@naver.com>
2023-11-08 21:51:33 +0900

    awef
    awefawe

--- PassportExample2/app3.js
+++ PassportExample2/app3.js
@@ -163,75 +163,10 @@ app.use(flash());
 // =============패스포트 strategy 설정 ===================
 const localStrategy = require('passport-local').Strategy;
 
-passport.use('local-login',new localStrategy({
-  usernameField:'email',
-  passwordField:'password',
-  passReqToCallback:true,
-}, function(req,email, password,done){
-  console.log('passport의 local-login 호출됨. :' + email +  ', ' + password);
-
-  const database = app.get('database');
-  database.UserModel.findOne({'email': email}, function(err,user){
-    if(err){
-      return done(err);
-    } 
-
-    if (!user){
-      console.log('사용자 정보가 일치하지 않습니다');
-      return done(null, false, req.flash('loginMessage', '등록된 계정이 없습니다'));  
-    }
-
-    const authenticated = user.authenticate(password, user._doc.salt, user._doc.hashed_password);
-    if(!authenticated){
-      console.log('비밀번호가 일치하지 않습니다');
-      return done (null, false, req.flash('loginMessage', '비밀번호가 일치하지 않습니다'));
-
-    }
-
-    console.log('아이디와 비밀번호가 일치합니다')
-    return done(null, user);
-
-  });
-
-}));
+passport.use('local-login');
 
 
-passport.use('local-signup', new localStrategy({
-  usernameField:'email',
-  passwordField:'password',
-  passReqToCallBack: true,
-
-}, function(req, email, password, done){
-  const paramName = req.body.name || req.query.name;
-  console.log('passport의 local signup 호출됨' + email + ', '  + password + ', ' + 
-  paramName);
-
-  const database = app.get('database');
-  database.UserModel.findOne({'email':email}, function(err,user){
-    if(err){
-      console.log('에러 발생');
-      return done(err);
-    }
-
-    if(user){
-      console.log('기존에 계정이 있습니다')
-      return done(null, false, req.flash('signupMessage', '계정이 이미 있습니다'));
-
-    } else{
-      const user  = new database.UserModel({'email':email ,'password':password, 'name':paramName});
-      user.save(function(err){
-        if(err){
-          console.log('데이터베이스에 저장시 에러');
-          return done(null, false, req.flash('signupMessage', '사용자 ㅈ어보 저장시  에러가 발생했습니다'))
-        }
-
-        console.log('사용자 데이터 저장함')
-        return done(null, user);
-      });
-    }
-
-  });
-}))
+passport.use('local-signup');
 
 
 passport.serializeUser( function(user, done){
