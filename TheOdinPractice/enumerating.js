5402ea6b3559b60addb5f0e35cee66524aab074e
golbangi1000 <kenjames777@naver.com>
2023-11-20 18:34:14 +0900

    r3w
    faew

---
+++ TheOdinPractice/enumerating.js
@@ -0,0 +1,30 @@
+const circle = {
+   radius: 12,
+   draw: function () {
+      console.log("draw");
+   },
+};
+
+for (key in circle) {
+   console.log(key, circle[key]);
+}
+
+//for of only work on iterables like arrays and maps,but using these work
+console.log("==============");
+for (key of Object.keys(circle)) {
+   console.log(key);
+}
+for (entry of Object.entries(circle)) {
+   console.log(entry);
+}
+
+// use in for if statement to check what key is in the object like
+
+if ("radius" in circle) {
+   console.log("yes");
+}
+if ("radiuaasdfs" in circle) {
+   console.log("yes");
+} else {
+   console.log("no");
+}
