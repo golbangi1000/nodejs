99e5ffc534598f8d55d39a03d94a2311624304b3
golbangi1000 <kenjames777@naver.com>
2023-11-21 18:05:19 +0900

    aewfafw
    awefwaef

---
+++ TheOdinPractice/cloning.js
@@ -0,0 +1,21 @@
+const circle = {
+   radius: 12,
+   draw: function () {
+      console.log("draw");
+   },
+};
+
+// const another = {};
+
+// for (let key in circle) {
+//    another[key] = circle[key];
+// }
+
+// another["radius"] = circle["radius"];
+// console.log(another.draw);
+
+// const another = Object.assign({ color: "yellow" }, circle); // get all stuff in circle to the object on the left
+
+const another = { ...circle }; //takes all the properties and methods in the object best way to clone an object
+
+console.log(another);
