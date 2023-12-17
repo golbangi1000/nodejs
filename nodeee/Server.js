9faac4c379b5b35468095208755ef2a9ad718288
golbangi1000 <kenjames777@naver.com>
2023-12-17 18:09:52 +0900

    awef
    awfe

--- nodeee/Server.js
+++ nodeee/Server.js
@@ -4,17 +4,17 @@ const app = express();
 app.use(express.static(__dirname + "/public")); //css js jpg (static files)
 
 app.listen(8080, () => {
-   console.log("8080서버 실행중");
+	console.log("8080서버 실행중");
 });
 
 app.get("/", (req, res) => {
-   res.sendFile(__dirname + "/index.html");
+	res.sendFile(__dirname + "/index.html");
 });
 
 app.get("/hello", (req, res) => {
-   res.send("boomboom");
+	res.send("boomboom");
 });
 
-app.get("/shop", (req, res) => {
-   res.send("shop");
+app.get("/game", (req, res) => {
+	res.sendFile(__dirname + "/game.html");
 });
