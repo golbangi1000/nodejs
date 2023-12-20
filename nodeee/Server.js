defabed8b83cb4ccc8d779d31e8f335b32082de5
golbangi1000 <kenjames777@naver.com>
2023-12-20 21:51:39 +0900

    0223120120
    awefaw

--- nodeee/Server.js
+++ nodeee/Server.js
@@ -1,10 +1,29 @@
 const express = require("express");
+const { mongoClient, MongoClient } = require("mongodb");
+require("dotenv").config();
+
 const app = express();
 
 app.use(express.static(__dirname + "/public")); //css js jpg (static files)
 
-app.listen(8080, () => {
-	console.log("8080서버 실행중");
+const MONGO_URL = process.env.MONGO_URL;
+const PORT1 = process.env.PORT;
+const PORT = parseFloat(PORT1);
+
+let db;
+const url = MONGO_URL;
+new MongoClient(url)
+	.connect()
+	.then((client) => {
+		console.log("DB연결성공");
+		db = client.db("forum");
+	})
+	.catch((err) => {
+		console.log("mongodb connection error" + ":" + err);
+	});
+
+app.listen(PORT, () => {
+	console.log(8080 + "서버 실행중");
 });
 
 app.get("/", (req, res) => {
@@ -17,4 +36,15 @@ app.get("/hello", (req, res) => {
 
 app.get("/game", (req, res) => {
 	res.sendFile(__dirname + "/game.html");
+});
+
+app.get("/newbook", (req, res) => {
+	db.collection("post").insertOne({ title: "book1234" });
+});
+
+app.get("/list", async (req, res) => {
+	let documentResult = await db.collection("post").find().toArray(); //wait till it finishes
+
+	console.log(documentResult);
+	res.send("DB document에 있던것들");
 });
