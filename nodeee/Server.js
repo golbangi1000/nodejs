bf865bc185294f3d5763ad0f54b694067c8f1c99
golbangi1000 <kenjames777@naver.com>
2023-12-28 18:18:38 +0900

    20231228
    12awewf

--- nodeee/Server.js
+++ nodeee/Server.js
@@ -20,110 +20,110 @@ const PORT = parseFloat(PORT1);
 let db;
 const url = MONGO_URL;
 new MongoClient(url)
-	.connect()
-	.then((client) => {
-		console.log("DB연결성공");
+   .connect()
+   .then((client) => {
+      console.log("DB연결성공");
 
-		db = client.db("forum");
-	})
-	.catch((err) => {
-		console.log("mongodb connection error" + ":" + err);
-	});
+      db = client.db("forum");
+   })
+   .catch((err) => {
+      console.log("mongodb connection error" + ":" + err);
+   });
 
 app.listen(PORT, () => {
-	console.log(8080 + "서버 실행중");
+   console.log(8080 + "서버 실행중");
 });
 
 app.get("/", (req, res) => {
-	res.sendFile(__dirname + "/index.html");
+   res.sendFile(__dirname + "/index.html");
 });
 
 app.get("/game", (req, res) => {
-	res.sendFile(__dirname + "/game.html");
+   res.sendFile(__dirname + "/game.html");
 });
 
 app.get("/write", (req, res) => {
-	res.render("write.ejs");
+   res.render("write.ejs");
 });
 
 app.post("/add", async (req, res) => {
-	// await db.collection("post").insertOne(req.body);
-	await db
-		.collection("post")
-		.insertOne({ title: req.body.title, content: req.body.content });
-	res.redirect("/list");
+   // await db.collection("post").insertOne(req.body);
+   await db.collection("post").insertOne({ title: req.body.title, content: req.body.content });
+   res.redirect("/list");
 });
 
 app.get("/list", async (req, res) => {
-	let documentResult = await db.collection("post").find().toArray(); //wait till it finishes
+   let documentResult = await db.collection("post").find().toArray(); //wait till it finishes
 
-	res.render("list.ejs", { posts: documentResult });
+   res.render("list.ejs", { posts: documentResult });
 });
 
 // ":" 다음에 아무문자만 입력 (URL 파라미터)
 app.get("/detail/:id", async (req, res) => {
-	// _id : new ObjectId("6582d8d7af59763dd065615f");
-	try {
-		const userID = new ObjectId(req.params.id);
-		const result = await db.collection("post").findOne({ _id: userID });
-		res.render("detail.ejs", { post: result });
-	} catch (error) {
-		console.log(error);
-		res.status(404).send("wrong post id");
-	}
+   // _id : new ObjectId("6582d8d7af59763dd065615f");
+   try {
+      const userID = new ObjectId(req.params.id);
+      const result = await db.collection("post").findOne({ _id: userID });
+      res.render("detail.ejs", { post: result });
+   } catch (error) {
+      console.log(error);
+      res.status(404).send("wrong post id");
+   }
 });
 
 app.get("/edit/:id", async (req, res) => {
-	try {
-		const result = await db
-			.collection("post")
-			.findOne({ _id: new ObjectId(req.params.id) });
-		res.render("edit.ejs", { editPost: result });
-	} catch (error) {
-		console.log(error);
+   try {
+      const result = await db.collection("post").findOne({ _id: new ObjectId(req.params.id) });
+      res.render("edit.ejs", { editPost: result });
+   } catch (error) {
+      console.log(error);
 
-		res.redirect("/list");
-	}
+      res.redirect("/list");
+   }
 });
 
 app.put("/edit/:id", async (req, res) => {
-	const result = await db
-		.collection("post")
-		.updateOne(
-			{ _id: new ObjectId(req.params.id) },
-			{ $set: { title: req.body.title, content: req.body.content } }
-		);
-	res.redirect("/list");
+   const result = await db
+      .collection("post")
+      .updateOne(
+         { _id: new ObjectId(req.params.id) },
+         { $set: { title: req.body.title, content: req.body.content } }
+      );
+   res.redirect("/list");
 });
 
 app.delete("/delete/:id", async (req, res) => {
-	console.log(req.params.id);
-	const result = db
-		.collection("post")
-		.deleteOne({ _id: new ObjectId(req.params.id) });
-	res.send("삭제완료");
+   console.log(req.params.id);
+   const result = db.collection("post").deleteOne({ _id: new ObjectId(req.params.id) });
+   res.send("삭제완료");
 });
 
 // ============ use skip but it can be slow =============
 app.get("/list/:number", async (req, res) => {
-	let documentResult = await db
-		.collection("post")
-		.find()
-		.skip((req.params.number - 1) * 5)
-		.limit(5)
-		.toArray(); //wait till it finishes
+   let documentResult = await db
+      .collection("post")
+      .find()
+      .skip((req.params.number - 1) * 5)
+      .limit(5)
+      .toArray(); //wait till it finishes
 
-	res.render("list.ejs", { posts: documentResult });
+   res.render("list.ejs", { posts: documentResult });
 });
 
 app.get("/list/next/:number", async (req, res) => {
-	let documentResult = await db
-		.collection("post")
-		.find({ _id: { $gt: new ObjectId(req.params.number) } })
-		.limit(5)
-		.toArray(); //wait till it finishes
+   let documentResult = await db
+      .collection("post")
+      .find({ _id: { $gt: new ObjectId(req.params.number) } })
+      .limit(5)
+      .toArray(); //wait till it finishes
 
-	res.render("list.ejs", { posts: documentResult });
+   res.render("list.ejs", { posts: documentResult });
+});
+
+app.get("/login", async (req, res) => {
+   // await db.collection("post").insertOne(req.body);
+   await db.collection("post").insertOne({ title: req.body.title, content: req.body.content });
+   res.render("login.ejs");
 });
 
 //how to write a user post function
